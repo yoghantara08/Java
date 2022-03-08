0066e029a2b75fba7728534afbbbaae23de21812
Yoghantara <gyoghantara@gmail.com>
2022-03-08 22:06:28 +0800

    tipe data number (integer)

---
+++ Java Dasar/Tipe Data/Number/Integer.java
@@ -0,0 +1,16 @@
+package Number;
+
+public class Integer {
+
+    public static void main(String[] args) throws Exception {
+        // integer
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInteger = 1000000;
+        long iniLong = 1000000;
+        long iniLong2 = 1000000L; // L = Long
+
+        System.out.println(iniByte + iniShort + iniInteger + iniLong + iniLong2);
+    }
+    
+}
