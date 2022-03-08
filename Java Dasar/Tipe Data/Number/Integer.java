02b27977a1a43463580ddbc54dfaa586452ddde9
Yoghantara <gyoghantara@gmail.com>
2022-03-08 22:24:24 +0800

    Konversi Number

--- Java Dasar/Tipe Data/Number/Integer.java
+++ Java Dasar/Tipe Data/Number/Integer.java
@@ -19,22 +19,6 @@ public class Integer {
         long balance = 1_000_000_000L;
         int sum = 60_000_000;
 
-        // Konversi Tipe Data Number
-        // Widening Casting (Otomatis): byte -> short -> int -> long -> float -> double
-        byte Byte = 10;
-        short Short = Byte;
-        int Int = Short;
-        long Long = Int;
-        float Float = Long;
-        double Double = Float;
-
-        // Narrowing Casting (Manual): double -> float -> long -> int -> short -> byte
-        float Float2 = (float) Double;
-        long Long2 = (long) Float2;
-        int Int2 = (int) Long2;
-        short Short2 = (short) Int2;
-        byte Byte2 = (byte) Short2;
-
     }
     
 }
