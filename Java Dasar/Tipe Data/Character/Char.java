857a4858a245d654a9df2b3856c53c20dcb11df1
Yoghantara <gyoghantara@gmail.com>
2022-03-09 10:53:33 +0800

    tipe data character

---
+++ Java Dasar/Tipe Data/Character/Char.java
@@ -0,0 +1,18 @@
+public class Char {
+    
+    public static void main(String[] args) {
+        
+        char g = 'G';
+        char u = 'U';
+        char s = 'S';
+
+        System.out.println(g);
+        System.out.println(u);
+        System.out.println(s);
+
+    }
+
+}
+
+
+    
