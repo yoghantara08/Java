b8eb0b724d8f5d87584b2368ad8b7c8de6ffca6c
Yoghantara <gyoghantara@gmail.com>
2022-03-09 11:12:11 +0800

    tipe data string

---
+++ Java Dasar/Tipe Data/String/TipeDataString.java
@@ -0,0 +1,8 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Gustut";
+        String lastName = "Yoghantara";
+
+        System.out.println(firstName + " " + lastName);
+    }
+}
