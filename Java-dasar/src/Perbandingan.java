cbfb053dc630ba599f0a83a7cef083e00523f0d3
Yoghantara <gyoghantara@gmail.com>
2022-03-11 11:04:52 +0800

    Operasi Perbandingan

---
+++ Java-dasar/src/Perbandingan.java
@@ -0,0 +1,13 @@
+public class Perbandingan {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 20;
+
+        System.out.println(a > b); // True
+        System.out.println(a < b); // False
+        System.out.println(a >= b); // True
+        System.out.println(a <= b); // False
+        System.out.println(a == b);  // False
+        System.out.println(a != b); // True
+    }
+}
