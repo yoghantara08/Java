e9516b1775764935c8c911d624f1961312d6e152
Yoghantara <gyoghantara@gmail.com>
2022-03-12 13:19:10 +0800

    Method / Function

---
+++ Java-dasar/src/Method.java
@@ -0,0 +1,10 @@
+public class Method {
+    public static void main(String[] args) {
+        sayHelloWorld();
+    }
+
+    // Method = Function
+    static void sayHelloWorld(){
+        System.out.println("Hello World");
+    }
+}
