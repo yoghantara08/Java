512194deefb5904ae659803ead08f39a44fc4fc0
Yoghantara <gyoghantara@gmail.com>
2022-03-12 11:59:19 +0800

    block,statement,expression

---
+++ Java-dasar/src/Block.java
@@ -0,0 +1,12 @@
+public class Block {
+    // Block Scope
+    public static void main(String[] args) {
+        // Expression
+        int a;
+        a = 10;
+
+        // Statement
+        System.out.println(a);
+
+    }
+}
