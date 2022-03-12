8490105afff314ed0a2f97a0541747d2474574a8
Yoghantara <gyoghantara@gmail.com>
2022-03-12 12:44:16 +0800

    For Loop

---
+++ Java-dasar/src/For_Loop.java
@@ -0,0 +1,12 @@
+public class For_Loop {
+    public static void main(String[] args) {
+        for (var nilai = 1; nilai <= 10; nilai++){
+            System.out.println(nilai);
+        }
+
+        // Infinite Loop
+        // for(var nilai = 1; ;nilai++){
+        //     System.out.println(nilai);
+        // }
+    }
+}
