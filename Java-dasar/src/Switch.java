e45e8f450f34042da10f52b683a72c9cbff2f33d
Yoghantara <gyoghantara@gmail.com>
2022-03-12 12:25:51 +0800

    Switch Statement & Switch Lambda

---
+++ Java-dasar/src/Switch.java
@@ -0,0 +1,33 @@
+public class Switch {
+    public static void main(String[] args) {
+        // Switch Statement
+        var nilai = "B";
+        switch (nilai){
+            case "A":
+                System.out.println("Nilai A");
+                break;
+            case "B":
+                System.out.println("Nilai B");
+                break;
+            case "C":
+            case "D":
+                System.out.println("Nilai C & D");
+                break;
+            default:
+                System.out.println("Salah Memasukkan Nilai");
+                break;
+
+            }
+            
+        // Switch Lambda
+        var lulus = nilai;
+        switch (lulus) {
+            case "A" -> System.out.println("Anda Lulus");
+            case "B" -> System.out.println("Anda Lulus");
+            case "C", "D" -> System.out.println("Gagal");
+            default -> {
+                System.out.println("Salah Memasukkan Nilai");
+            }
+        }
+    }
+}
