137bac50e42e81a02181e59de85b545b7c4f2024
Yoghantara <gyoghantara@gmail.com>
2022-03-12 12:49:21 +0800

    do while loop

---
+++ Java-dasar/src/DoWhile_Loop.java
@@ -0,0 +1,10 @@
+public class DoWhile_Loop {
+    public static void main(String[] args) {
+        var nilai = 100;
+
+        do {
+            System.out.println(nilai);
+            nilai++;
+        } while (nilai < 10);
+    }
+}
