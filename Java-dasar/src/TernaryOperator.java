b188dee2d017fa76852ed7eb78de4adc80c4739a
Yoghantara <gyoghantara@gmail.com>
2022-03-12 12:36:03 +0800

    ternary operator

---
+++ Java-dasar/src/TernaryOperator.java
@@ -0,0 +1,10 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        var nilai = 75;
+        String ucapan;
+
+        ucapan = nilai >= 75 ? "LULUS" : "GAGAL";
+        
+        System.out.println(ucapan);
+    }
+}
