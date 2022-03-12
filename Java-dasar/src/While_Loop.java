651cd4608efc2761f0798e6c46ea4a75128b85a5
Yoghantara <gyoghantara@gmail.com>
2022-03-12 12:46:24 +0800

    while loop

---
+++ Java-dasar/src/While_Loop.java
@@ -0,0 +1,10 @@
+public class While_Loop {
+    public static void main(String[] args) {
+        var nilai = 1;
+
+        while(nilai < 10){
+            System.out.println(nilai);
+            nilai++;
+        }
+    }
+}
