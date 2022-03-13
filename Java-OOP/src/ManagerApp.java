cec62d8a20a28dd497435f0a301f2dcfe94169ff
Yoghantara <gyoghantara@gmail.com>
2022-03-13 12:19:20 +0800

    super constructor

--- Java-OOP/src/ManagerApp.java
+++ Java-OOP/src/ManagerApp.java
@@ -1,11 +1,9 @@
 public class ManagerApp {
     public static void main(String[] args) {
-        var manager = new Manager_Inheritance();
-        manager.name = "Gustut";
+        var manager = new Manager_Inheritance("Gustut");
         manager.sayHello("Ricky");
 
-        var pegawai = new Pegawai();
-        pegawai.name = "Yoghantara";
+        var pegawai = new Pegawai("Yoghantara");
         pegawai.sayHello("Galang");
     }
 }
--- Java-OOP/src/Manager_Inheritance.java
+++ Java-OOP/src/Manager_Inheritance.java
@@ -1,14 +1,29 @@
 class Manager_Inheritance {
     String name;
+    String company;
 
     void sayHello(String name){
         System.out.println("Hi " + name + ", Saya Manager " + this.name);
     }
     
+    // Constructor 
+    Manager_Inheritance(String name){
+        this.name = name;
+    }
+    Manager_Inheritance(String name, String company){
+        this.name = name;
+        this.company = company;
+    }
 }
 
 // Bisa buat file berbeda
 class Pegawai extends Manager_Inheritance {
+    // Class Child harus memanggil satu constructor (yg ada parameter)
+    // Class child tidak perlu memanggil constructor jika di class parent mendeklarasikan default constructor
+    Pegawai(String name) {
+        super(name);
+    }
+
     // Method Overriding
     void sayHello(String name){
         System.out.println("Hallo " + name + ", Saya Pegawai " + this.name);
