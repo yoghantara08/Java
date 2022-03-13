68f2dc5ec2850a374b6d3abc86f7c3dd3f09c3e0
Yoghantara <gyoghantara@gmail.com>
2022-03-13 11:53:06 +0800

    method overriding

--- Java-OOP/src/Manager_Inheritance.java
+++ Java-OOP/src/Manager_Inheritance.java
@@ -2,12 +2,15 @@ class Manager_Inheritance {
     String name;
 
     void sayHello(String name){
-        System.out.println("Hi " + name + ", Nama Saya " + this.name);
+        System.out.println("Hi " + name + ", Saya Manager " + this.name);
     }
     
 }
 
 // Bisa buat file berbeda
 class Pegawai extends Manager_Inheritance {
-
+    // Method Overriding
+    void sayHello(String name){
+        System.out.println("Hallo " + name + ", Saya Pegawai " + this.name);
+    }
 }
