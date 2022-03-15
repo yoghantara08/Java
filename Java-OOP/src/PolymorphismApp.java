ceb18117b92592266ce20e990fdfde12d94d9fa4
Yoghantara <gyoghantara@gmail.com>
2022-03-15 13:44:22 +0800

    Type check and cast

--- Java-OOP/src/PolymorphismApp.java
+++ Java-OOP/src/PolymorphismApp.java
@@ -16,6 +16,14 @@ public class PolymorphismApp {
     }
 
     static void sayHello(Employee employee){
-        System.out.println("Hello " + employee.name);
+        if (employee instanceof Pegawai){
+            Pegawai pegawai = (Pegawai) employee;
+            System.out.println("Hello Pegawai " + pegawai.name);
+        } else if (employee instanceof Manager){
+            Manager manager = (Manager) employee;
+            System.out.println("Hello Manager " + manager.name);
+        } else {
+            System.out.println("Hello " + employee.name);
+        }
     }
 }
