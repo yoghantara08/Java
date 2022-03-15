afe54339bc7ff5a4a5ffdd1032530a1f9e532f14
Yoghantara <gyoghantara@gmail.com>
2022-03-15 14:04:36 +0800

    Package

---
+++ Java-OOP/src/PackageJava/app/Main.java
@@ -0,0 +1,7 @@
+package PackageJava.app;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Java-OOP/src/PackageJava/data/Product.java
@@ -0,0 +1,11 @@
+package PackageJava.data;
+
+class Product {
+    String name;
+    int price;
+
+    Product(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+}
