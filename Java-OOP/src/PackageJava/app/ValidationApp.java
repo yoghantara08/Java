502503b7f1a499126d59b13e695ce362c9c5b329
Yoghantara <gyoghantara@gmail.com>
2022-03-17 20:41:12 +0800

    Exception / Error handling

---
+++ Java-OOP/src/PackageJava/app/ValidationApp.java
@@ -0,0 +1,35 @@
+package PackageJava.app;
+
+import PackageJava.data.LoginRequest;
+import PackageJava.error.ValidationException;
+import PackageJava.util.ValidationUtil;
+
+public class ValidationApp {
+    public static void main(String[] args) throws ValidationException {
+
+        // ERROR
+        // LoginRequest loginRequest = new LoginRequest(null, "");
+        // Valid
+        LoginRequest loginRequest = new LoginRequest("Gustut", "Pass");
+
+        try {
+            ValidationUtil.validate(loginRequest);
+            System.out.println("Data valid"); 
+        } catch (ValidationException | NullPointerException err) {
+            System.out.println("Data tidak valid : " + err.getMessage());
+        } finally {
+            System.out.println("Finally selalu di eksekusi");
+        }
+        
+        // try {
+        //     ValidationUtil.validate(loginRequest);
+        //     System.out.println("Data valid"); 
+        // } catch (ValidationException err) {
+        //     System.out.println("Data tidak valid : " + err.getMessage());
+        // } catch (NullPointerException n){
+        //     System.out.println("Data null : " + n.getMessage());
+        // }
+
+    }
+
+}
---
+++ Java-OOP/src/PackageJava/error/ValidationException.java
@@ -0,0 +1,9 @@
+package PackageJava.error;
+
+public class ValidationException extends Throwable{
+
+    public ValidationException(String message){
+        super(message);
+    }
+
+}
