270463de4defac3d67d49721b2a451aa512af5d5
Yoghantara <gyoghantara@gmail.com>
2022-05-24 15:43:55 +0800

    innitialize apache-maven

---
+++ Java-ApacheMaven/src/main/java/yoghantara/maven/App.java
@@ -0,0 +1,13 @@
+package yoghantara.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
