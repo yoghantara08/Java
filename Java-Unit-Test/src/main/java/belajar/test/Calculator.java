d98e450c63b33477b5a12fed7c0521996b898f9e
Yoghantara <gyoghantara@gmail.com>
2022-05-24 22:44:54 +0800

    Assertions

--- Java-Unit-Test/src/main/java/belajar/test/Calculator.java
+++ Java-Unit-Test/src/main/java/belajar/test/Calculator.java
@@ -6,4 +6,12 @@ public class Calculator {
     return numb1 + numb2;
   }
 
+  public Integer divide(Integer numb1, Integer numb2) {
+    if (numb2 == 0) {
+      throw new IllegalArgumentException("Can't divide by zero");
+    } else {
+      return numb1 / numb2;
+    }
+  }
+
 }
--- Java-Unit-Test/src/test/java/belajar/test/CalculatorTest.java
+++ Java-Unit-Test/src/test/java/belajar/test/CalculatorTest.java
@@ -1,6 +1,7 @@
 package belajar.test;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
@@ -9,6 +10,19 @@ public class CalculatorTest {
   @Test
   public void testSumSuccess() {
     var result = calculator.sum(436, 10);
+    assertEquals(446, result);
   }
 
+  @Test
+  public void testDivideSuccess() {
+    var result = calculator.divide(100, 10);
+    assertEquals(10, result);
+  }
+
+  @Test
+  public void testDivideFailed() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      calculator.divide(100, 0);
+    });
+  }
 }
