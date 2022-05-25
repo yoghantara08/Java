d8ba01fc26bccbe08ca854bb59678316fc4c80ca
Yoghantara <gyoghantara@gmail.com>
2022-05-25 11:06:09 +0800

    DisplayName

--- Java-Unit-Test/src/test/java/belajar/test/CalculatorTest.java
+++ Java-Unit-Test/src/test/java/belajar/test/CalculatorTest.java
@@ -1,25 +1,35 @@
 package belajar.test;
 
 import org.junit.jupiter.api.Test;
+
+import belajar.test.generator.SimpleDisplayNameGenerator;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+
+@DisplayNameGeneration(SimpleDisplayNameGenerator.class)
 public class CalculatorTest {
 
   private Calculator calculator = new Calculator();
 
   @Test
+  @DisplayName("Test sum success")
   public void testSumSuccess() {
     var result = calculator.sum(436, 10);
     assertEquals(446, result);
   }
 
   @Test
+  @DisplayName("Test divide success")
   public void testDivideSuccess() {
     var result = calculator.divide(100, 10);
     assertEquals(10, result);
   }
 
   @Test
+  @DisplayName("Test failed to divided by zero")
   public void testDivideFailed() {
     assertThrows(IllegalArgumentException.class, () -> {
       calculator.divide(100, 0);
---
+++ Java-Unit-Test/src/test/java/belajar/test/generator/SimpleDisplayNameGenerator.java
@@ -0,0 +1,23 @@
+package belajar.test.generator;
+
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+public class SimpleDisplayNameGenerator implements DisplayNameGenerator {
+
+  @Override
+  public String generateDisplayNameForClass(Class<?> testClass) {
+    return "Test " + testClass.getSimpleName();
+  }
+
+  @Override
+  public String generateDisplayNameForMethod(Class<?> testClass, Method testMethod) {
+    return "Test " + testClass.getSimpleName() + "." + testMethod.getName();
+  }
+
+  @Override
+  public String generateDisplayNameForNestedClass(Class<?> nestedClass) {
+    return null;
+  }
+
+}
